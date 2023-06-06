7a46388e7216247ab8627f3e5a364c4d4c97511b
Rodrigo Gutierrez <rodrigo.gutierrez.acc@gmail.com>
2023-06-06 00:20:59 -0400

    Added costos directos manufactura y servicios tables

--- frontend-planDeNegocio/src/app/pages/costos/costos.component.ts
+++ frontend-planDeNegocio/src/app/pages/costos/costos.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./costos.component.scss']
 })
 export class CostosComponent {
+  public colSize!: number;
+  public colSize2: number = 1;
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+  ) { }
+  ngOnInit() {
+    this.breakpointObserver.observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium])
+      .subscribe(result => {
+        if (result.matches) {
+          this.colSize = this.calculateColSize(result.breakpoints);
+          this.colSize2 = this.calculateColSize2(result.breakpoints);
+        } else {
+          this.colSize = 3; // Pantallas más grandes, 4 columnas
+          this.colSize2 = 1;
+        }
+      });
+  }
+  calculateColSize(breakpoints: { [key: string]: boolean }): number {
+    if (breakpoints[Breakpoints.XSmall]) {
+      return 2; // Pantallas extra pequeñas, 1 columna
+    } else if (breakpoints[Breakpoints.Small]) {
+      return 2; // Pantallas pequeñas, 2 columnas
+    } else if (breakpoints[Breakpoints.Medium]) {
+      return 3; // Pantallas medianas, 3 columnas
+    } else {
+      return 3; // Por defecto, 4 columnas
+    }
+  }
+  calculateColSize2(breakpoints: { [key: string]: boolean }): number {
+    if (breakpoints[Breakpoints.XSmall]) {
+      return 2; // Pantallas extra pequeñas, 1 columna
+    } else if (breakpoints[Breakpoints.Small]) {
+      return 2; // Pantallas pequeñas, 2 columnas
+    } else if (breakpoints[Breakpoints.Medium]) {
+      return 1; // Pantallas medianas, 3 columnas
+    } else {
+      return 1; // Por defecto, 4 columnas
+    }
+  }
+  rows: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  inputValues: (number | null)[] = [];
+  total: number = 0;
 
+  calculateTotal(): void {
+    this.total = this.inputValues
+      .map(value => parseInt(value?.toString() || '0') || 0)
+      .reduce((sum, value) => sum + value, 0);
+  }
 }
