94ac79a2f6258c799a9a744d8bd0b5669e68d76c
Richar Irasabal <irasabal.richy@gmail.com>
2023-06-08 14:46:23 -0400

    service para home agregado

--- frontend-planDeNegocio/src/app/service/home/home.service.spec.ts
+++ frontend-planDeNegocio/src/app/service/home/home.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Pregunta3Service } from './pregunta3.service';
+import { HomeService } from './home.service';
 
-describe('Pregunta3Service', () => {
-  let service: Pregunta3Service;
+describe('HomeService', () => {
+  let service: HomeService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Pregunta3Service);
+    service = TestBed.inject(HomeService);
   });
 
   it('should be created', () => {
--- frontend-planDeNegocio/src/app/service/home/home.service.ts
+++ frontend-planDeNegocio/src/app/service/home/home.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class Pregunta3Service {
+export class HomeService {
 
   constructor() { }
 }
