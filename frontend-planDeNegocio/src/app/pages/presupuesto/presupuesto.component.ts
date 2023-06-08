38c865bf96977139cad2cf02cc6d5e5c7f336c5c
Richar Irasabal <irasabal.richy@gmail.com>
2023-06-07 23:46:07 -0400

    calculos de presupuesto terminado

--- frontend-planDeNegocio/src/app/pages/presupuesto/presupuesto.component.ts
+++ frontend-planDeNegocio/src/app/pages/presupuesto/presupuesto.component.ts
@@ -11,6 +11,42 @@ export class PresupuestoComponent {
     private presupuestoService: PresupuestoService
   ) { }
 
+  totalCapitalOpApPro: number = 0;
+  totalCapitalOpInvertira: number = 0;
+
+  totalCapitalInversionApPo!: number;
+  totalCapitalInversionSeInvertira!: number;
+
+  calcularTotalOperaciones() {
+    this.totalCapitalOpApPro =
+      (this.presupuestoService.getTotalMateriaPrima() ?? 0) +
+      (this.presupuestoService.getTotalReqProm() ?? 0) +
+      (this.presupuestoService.getValorManoObra() ?? 0);
+
+    this.presupuestoService.setTotalCapitalOpApPro(this.totalCapitalOpApPro);
+  }
+  calcularTotalOperacionesInvertira() {
+    this.totalCapitalOpInvertira = (this.presupuestoService.getTotalMateriaPrima1() ?? 0) +
+      (this.presupuestoService.getTotalReqProm1() ?? 0) +
+      (this.presupuestoService.getTotalGastosOp() ?? 0);
+
+    this.presupuestoService.setTotalCapitalOpInvertira(this.totalCapitalOpInvertira);
+  }
+  calcularCapitalInversionApPro(){
+    this.totalCapitalInversionApPo =
+      (this.presupuestoService.getTotalInfr() ?? 0) +
+      (this.presupuestoService.getTotalMaq() ?? 0) +
+      (this.presupuestoService.getTotalLegal() ?? 0);
+      this.presupuestoService.setTotalCapitalInversionApPo(this.totalCapitalInversionApPo);
+  }
+  calcularCapitalInversionSeInvertira(){
+    this.totalCapitalInversionSeInvertira =
+      (this.presupuestoService.getTotalInfr1() ?? 0) +
+      (this.presupuestoService.getTotalMaq1() ?? 0) +
+      (this.presupuestoService.getTotalLegal1() ?? 0);
+      this.presupuestoService.setTotalCapitalInversionSeInvertira(this.totalCapitalInversionSeInvertira);
+  }
+
   //<input type="number" class="no-margin right-align" (change)="calculateTotal()" [(ngModel)]="inputValues[2]"/>
   // 8.1 detalle
   inputValuesDetalle: (number | null)[] = [];
@@ -34,6 +70,7 @@ export class PresupuestoComponent {
         this.presupuestoService.setInputValuesDetalle2(this.input1);
         break;
     }
+    this.calcularTotalOperaciones();
   }
   ngOnInit() {
     this.inputValuesDetalle[0] = this.presupuestoService.getInputValuesDetalle0();
@@ -61,6 +98,46 @@ export class PresupuestoComponent {
     this.inputValuesReqProm1[1] = this.presupuestoService.getInputValuesReqProm11();
     this.inputValuesReqProm1[2] = this.presupuestoService.getInputValuesReqProm12();
     this.totalReqProm1 = this.presupuestoService.getTotalReqProm1();
+
+    this.inputValuesGastosOp[0] = this.presupuestoService.getInputValuesGastosOp0();
+    this.inputValuesGastosOp[1] = this.presupuestoService.getInputValuesGastosOp1();
+    this.inputValuesGastosOp[2] = this.presupuestoService.getInputValuesGastosOp2();
+    this.totalGastosOp = this.presupuestoService.getTotalGastosOp();
+
+    this.inputValuesInfr[0] = this.presupuestoService.getInputValuesInfr0();
+    this.inputValuesInfr[1] = this.presupuestoService.getInputValuesInfr1();
+    this.inputValuesInfr[2] = this.presupuestoService.getInputValuesInfr2();
+    this.totalInfr = this.presupuestoService.getTotalInfr();
+
+    this.inputValuesInfr1[0] = this.presupuestoService.getInputValuesInfr10();
+    this.inputValuesInfr1[1] = this.presupuestoService.getInputValuesInfr11();
+    this.inputValuesInfr1[2] = this.presupuestoService.getInputValuesInfr12();
+    this.totalInfr1 = this.presupuestoService.getTotalInfr1();
+
+    this.inputValuesMaq[0] = this.presupuestoService.getInputValuesMaq0();
+    this.inputValuesMaq[1] = this.presupuestoService.getInputValuesMaq1();
+    this.inputValuesMaq[2] = this.presupuestoService.getInputValuesMaq2();
+    this.totalMaq = this.presupuestoService.getTotalMaq();
+
+    this.inputValuesMaq1[0] = this.presupuestoService.getInputValuesMaq10();
+    this.inputValuesMaq1[1] = this.presupuestoService.getInputValuesMaq11();
+    this.inputValuesMaq1[2] = this.presupuestoService.getInputValuesMaq12();
+    this.totalMaq1 = this.presupuestoService.getTotalMaq1();
+
+    this.inputValuesLegal[0] = this.presupuestoService.getInputValuesLegal0();
+    this.inputValuesLegal[1] = this.presupuestoService.getInputValuesLegal1();
+    this.inputValuesLegal[2] = this.presupuestoService.getInputValuesLegal2();
+    this.totalLegal = this.presupuestoService.getTotalLegal();
+
+    this.inputValuesLegal1[0] = this.presupuestoService.getInputValuesLegal10();
+    this.inputValuesLegal1[1] = this.presupuestoService.getInputValuesLegal11();
+    this.inputValuesLegal1[2] = this.presupuestoService.getInputValuesLegal12();
+    this.totalLegal1 = this.presupuestoService.getTotalLegal1();
+
+    this.totalCapitalOpApPro = this.presupuestoService.getTotalCapitalOpApPro();
+    this.totalCapitalOpInvertira = this.presupuestoService.getTotalCapitalOpInvertira();
+    this.totalCapitalInversionApPo = this.presupuestoService.getTotalCapitalInversionApPo();
+    this.totalCapitalInversionSeInvertira = this.presupuestoService.getTotalCapitalInversionSeInvertira();
   }
   // 8.2 Mano de obra <input type="number" (change)="agregarTotal()" [(ngModel)]="manoObraEmprendedor">
   manoObraEmprendedor!: number;
@@ -68,6 +145,7 @@ export class PresupuestoComponent {
   agregarTotal(): void {
     this.valorManoObra = this.manoObraEmprendedor;
     this.presupuestoService.setValorManoObra(this.valorManoObra);
+    this.calcularTotalOperaciones();
   }
   // 8.3 Materia prima, insumos y/o animales de engorde APORTE PROPIO
   inputValuesMateriaPrima: (number | null)[] = [];
@@ -91,7 +169,7 @@ export class PresupuestoComponent {
         this.presupuestoService.setInputValuesMateriaPrima2(this.input2);
         break;
     }
-
+    this.calcularTotalOperaciones();
   }
   // se INVERTIRA
   inputValuesMateriaPrima1: (number | null)[] = [];
@@ -114,113 +192,214 @@ export class PresupuestoComponent {
         this.presupuestoService.setInputValuesMateriaPrima12(this.input2);
         break;
     }
+    this.calcularTotalOperacionesInvertira();
   }
   // 8.4 Requerimientos Promocionales APORTE PROPIO
   inputValuesReqProm: (number | null)[] = [];
   totalReqProm: number = 0;
-  input3!: number;
+  // input3!: number;
 
   calculateTotal3(index: number): void {
-    this.input3 = this.inputValuesReqProm[index] ?? 0;
+    this.input2 = this.inputValuesReqProm[index] ?? 0;
     this.totalReqProm = this.inputValuesReqProm
       .map(value => parseInt(value?.toString() || '0') || 0)
       .reduce((sum, value) => sum + value, 0);
     this.presupuestoService.setTotalReqProm(this.totalReqProm);
     switch (index) {
       case 0:
-        this.presupuestoService.setInputValuesReqProm0(this.input3);
+        this.presupuestoService.setInputValuesReqProm0(this.input2);
         break;
       case 1:
-        this.presupuestoService.setInputValuesReqProm1(this.input3);
+        this.presupuestoService.setInputValuesReqProm1(this.input2);
         break;
       case 2:
-        this.presupuestoService.setInputValuesReqProm2(this.input3);
+        this.presupuestoService.setInputValuesReqProm2(this.input2);
         break;
     }
+    this.calcularTotalOperaciones();
   }
   // se INVERTIRA
   inputValuesReqProm1: (number | null)[] = [];
   totalReqProm1: number = 0;
 
   calculateTotal4(index: number): void {
-    this.input3 = this.inputValuesReqProm1[index] ?? 0;
+    this.input2 = this.inputValuesReqProm1[index] ?? 0;
     this.totalReqProm1 = this.inputValuesReqProm1
       .map(value => parseInt(value?.toString() || '0') || 0)
       .reduce((sum, value) => sum + value, 0);
     this.presupuestoService.setTotalReqProm1(this.totalReqProm1);
     switch (index) {
       case 0:
-        this.presupuestoService.setInputValuesReqProm10(this.input3);
+        this.presupuestoService.setInputValuesReqProm10(this.input2);
         break;
       case 1:
-        this.presupuestoService.setInputValuesReqProm11(this.input3);
+        this.presupuestoService.setInputValuesReqProm11(this.input2);
         break;
       case 2:
-        this.presupuestoService.setInputValuesReqProm12(this.input3);
+        this.presupuestoService.setInputValuesReqProm12(this.input2);
         break;
     }
+    this.calcularTotalOperacionesInvertira();
   }
   // 8.5 Gastos operativos SE INVERTIRA
   inputValuesGastosOp: (number | null)[] = [];
   totalGastosOp: number = 0;
 
-  calculateTotal5(): void {
+  calculateTotal5(index: number): void {
+    this.input2 = this.inputValuesGastosOp[index] ?? 0;
     this.totalGastosOp = this.inputValuesGastosOp
       .map(value => parseInt(value?.toString() || '0') || 0)
       .reduce((sum, value) => sum + value, 0);
+    this.presupuestoService.setTotalGastosOp(this.totalGastosOp);
+    switch (index) {
+      case 0:
+        this.presupuestoService.setInputValuesGastosOp0(this.input2);
+        break;
+      case 1:
+        this.presupuestoService.setInputValuesGastosOp1(this.input2);
+        break;
+      case 2:
+        this.presupuestoService.setInputValuesGastosOp2(this.input2);
+        break;
+    }
+    this.calcularTotalOperacionesInvertira();
   }
   // 8.6 Infraestructura y/o terrenos y/o plantines APORTE PROPIO
   inputValuesInfr: (number | null)[] = [];
   totalInfr: number = 0;
 
-  calculateTotal6(): void {
+  calculateTotal6(index: number): void {
+    this.input2 = this.inputValuesInfr[index] ?? 0;
     this.totalInfr = this.inputValuesInfr
       .map(value => parseInt(value?.toString() || '0') || 0)
       .reduce((sum, value) => sum + value, 0);
+    this.presupuestoService.setTotalInfr(this.totalInfr);
+    switch (index) {
+      case 0:
+        this.presupuestoService.setInputValuesInfr0(this.input2);
+        break;
+      case 1:
+        this.presupuestoService.setInputValuesInfr1(this.input2);
+        break;
+      case 2:
+        this.presupuestoService.setInputValuesInfr2(this.input2);
+        break;
+    }
+    this.calcularCapitalInversionApPro();
   }
   // se INVERTIRA
   inputValuesInfr1: (number | null)[] = [];
   totalInfr1: number = 0;
 
-  calculateTotal7(): void {
+  calculateTotal7(index: number): void {
+    this.input2 = this.inputValuesInfr1[index] ?? 0;
     this.totalInfr1 = this.inputValuesInfr1
       .map(value => parseInt(value?.toString() || '0') || 0)
       .reduce((sum, value) => sum + value, 0);
+    this.presupuestoService.setTotalInfr1(this.totalInfr1);
+    switch (index) {
+      case 0:
+        this.presupuestoService.setInputValuesInfr10(this.input2);
+        break;
+      case 1:
+        this.presupuestoService.setInputValuesInfr11(this.input2);
+        break;
+      case 2:
+        this.presupuestoService.setInputValuesInfr12(this.input2);
+        break;
+    }
+    this.calcularCapitalInversionSeInvertira();
   }
   // 8.7 Maquinaria, Equipos, Vehículos y/o Ganado
   inputValuesMaq: (number | null)[] = [];
   totalMaq: number = 0;
 
-  calculateTotal8(): void {
+  calculateTotal8(index: number): void {
+    this.input2 = this.inputValuesMaq[index] ?? 0;
     this.totalMaq = this.inputValuesMaq
       .map(value => parseInt(value?.toString() || '0') || 0)
       .reduce((sum, value) => sum + value, 0);
+    this.presupuestoService.setTotalMaq(this.totalMaq);
+    switch (index) {
+      case 0:
+        this.presupuestoService.setInputValuesMaq0(this.input2);
+        break;
+      case 1:
+        this.presupuestoService.setInputValuesMaq1(this.input2);
+        break;
+      case 2:
+        this.presupuestoService.setInputValuesMaq2(this.input2);
+        break;
+    }
+    this.calcularCapitalInversionApPro();
   }
   // se INVERTIRA
   inputValuesMaq1: (number | null)[] = [];
   totalMaq1: number = 0;
 
-  calculateTotal9(): void {
+  calculateTotal9(index: number): void {
+    this.input2 = this.inputValuesMaq1[index] ?? 0;
     this.totalMaq1 = this.inputValuesMaq1
       .map(value => parseInt(value?.toString() || '0') || 0)
       .reduce((sum, value) => sum + value, 0);
+    this.presupuestoService.setTotalMaq1(this.totalMaq1);
+    switch (index) {
+      case 0:
+        this.presupuestoService.setInputValuesMaq10(this.input2);
+        break;
+      case 1:
+        this.presupuestoService.setInputValuesMaq11(this.input2);
+        break;
+      case 2:
+        this.presupuestoService.setInputValuesMaq12(this.input2);
+        break;
+    }
+    this.calcularCapitalInversionSeInvertira();
   }
   // 8.8 Requirimientos Legales APORTE PROPIO
   inputValuesLegal: (number | null)[] = [];
   totalLegal: number = 0;
 
-  calculateTotal10(): void {
+  calculateTotal10(index: number): void {
+    this.input2 = this.inputValuesLegal[index] ?? 0;
     this.totalLegal = this.inputValuesLegal
       .map(value => parseInt(value?.toString() || '0') || 0)
       .reduce((sum, value) => sum + value, 0);
+    this.presupuestoService.setTotalLegal(this.totalLegal);
+    switch (index) {
+      case 0:
+        this.presupuestoService.setInputValuesLegal0(this.input2);
+        break;
+      case 1:
+        this.presupuestoService.setInputValuesLegal1(this.input2);
+        break;
+      case 2:
+        this.presupuestoService.setInputValuesLegal2(this.input2);
+        break;
+    }
+    this.calcularCapitalInversionApPro();
   }
   // se INVERTIRA
   inputValuesLegal1: (number | null)[] = [];
   totalLegal1: number = 0;
 
-  calculateTotal11(): void {
+  calculateTotal11(index: number): void {
+    this.input2 = this.inputValuesLegal1[index] ?? 0;
     this.totalLegal1 = this.inputValuesLegal1
       .map(value => parseInt(value?.toString() || '0') || 0)
       .reduce((sum, value) => sum + value, 0);
+      this.presupuestoService.setTotalLegal1(this.totalLegal1);
+      switch (index) {
+        case 0:
+          this.presupuestoService.setInputValuesLegal10(this.input2);
+          break;
+        case 1:
+          this.presupuestoService.setInputValuesLegal11(this.input2);
+          break;
+        case 2:
+          this.presupuestoService.setInputValuesLegal12(this.input2);
+          break;
+      }
+      this.calcularCapitalInversionSeInvertira();
   }
 }
--- frontend-planDeNegocio/src/app/service/presupuesto/presupuesto.service.ts
+++ frontend-planDeNegocio/src/app/service/presupuesto/presupuesto.service.ts
@@ -22,196 +22,382 @@ export class PresupuestoService {
 
   inputValuesDetalle!: number;
 
-  setInputValuesDetalle0(valor: number){
+  setInputValuesDetalle0(valor: number) {
     this.inputValuesDetalle = valor;
   }
-  getInputValuesDetalle0(){
+  getInputValuesDetalle0() {
     return this.inputValuesDetalle;
   }
 
   inputValuesDetalle1!: number;
 
-  setInputValuesDetalle1(valor: number){
+  setInputValuesDetalle1(valor: number) {
     this.inputValuesDetalle1 = valor;
   }
-  getInputValuesDetalle1(){
+  getInputValuesDetalle1() {
     return this.inputValuesDetalle1;
   }
 
   inputValuesDetalle2!: number;
 
-  setInputValuesDetalle2(valor: number){
+  setInputValuesDetalle2(valor: number) {
     this.inputValuesDetalle2 = valor;
   }
-  getInputValuesDetalle2(){
+  getInputValuesDetalle2() {
     return this.inputValuesDetalle2;
   }
-////////////////////////////////////////////////
-  setTotal(valor: number){
+  ////////////////////////////////////////////////
+  setTotal(valor: number) {
     this.total = valor;
   }
-  getTotal(){
+  getTotal() {
     return this.total;
   }
-  setValorManoObra(valor: number){
+  setValorManoObra(valor: number) {
     this.valorManoObra = valor;
   }
-  getValorManoObra(){
-    return this.valorManoObra
+  getValorManoObra() {
+    return this.valorManoObra;
   }
   /// 8.3
   inputvaluesMateriaPrima0!: number;
-  setInputValuesMateriaPrima0(valor: number){
+  setInputValuesMateriaPrima0(valor: number) {
     this.inputvaluesMateriaPrima0 = valor;
   }
-  getInputValuesMateriaPrima0(){
+  getInputValuesMateriaPrima0() {
     return this.inputvaluesMateriaPrima0;
   }
   inputvaluesMateriaPrima1!: number;
-  setInputValuesMateriaPrima1(valor: number){
+  setInputValuesMateriaPrima1(valor: number) {
     this.inputvaluesMateriaPrima1 = valor;
   }
-  getInputValuesMateriaPrima1(){
+  getInputValuesMateriaPrima1() {
     return this.inputvaluesMateriaPrima1;
   }
   inputvaluesMateriaPrima2!: number;
-  setInputValuesMateriaPrima2(valor: number){
+  setInputValuesMateriaPrima2(valor: number) {
     this.inputvaluesMateriaPrima2 = valor;
   }
-  getInputValuesMateriaPrima2(){
+  getInputValuesMateriaPrima2() {
     return this.inputvaluesMateriaPrima2;
   }
   //se invertira 8.3
   inputvaluesMateriaPrima10!: number;
-  setInputValuesMateriaPrima10(valor: number){
+  setInputValuesMateriaPrima10(valor: number) {
     this.inputvaluesMateriaPrima10 = valor;
   }
-  getInputValuesMateriaPrima10(){
+  getInputValuesMateriaPrima10() {
     return this.inputvaluesMateriaPrima10;
   }
   inputvaluesMateriaPrima11!: number;
-  setInputValuesMateriaPrima11(valor: number){
+  setInputValuesMateriaPrima11(valor: number) {
     this.inputvaluesMateriaPrima11 = valor;
   }
-  getInputValuesMateriaPrima11(){
+  getInputValuesMateriaPrima11() {
     return this.inputvaluesMateriaPrima11;
   }
   inputvaluesMateriaPrima12!: number;
-  setInputValuesMateriaPrima12(valor: number){
+  setInputValuesMateriaPrima12(valor: number) {
     this.inputvaluesMateriaPrima12 = valor;
   }
-  getInputValuesMateriaPrima12(){
+  getInputValuesMateriaPrima12() {
     return this.inputvaluesMateriaPrima12;
   }
 
-  setTotalMateriaPrima(valor: number){
+  setTotalMateriaPrima(valor: number) {
     this.totalMateriaPrima = valor;
   }
-  getTotalMateriaPrima(){
+  getTotalMateriaPrima() {
     return this.totalMateriaPrima;
   }
-  setTotalMateriaPrima1(valor: number){
+  setTotalMateriaPrima1(valor: number) {
     this.totalMateriaPrima1 = valor;
   }
-  getTotalMateriaPrima1(){
+  getTotalMateriaPrima1() {
     return this.totalMateriaPrima1;
   }
 
   // 8.4 inversion propia
   inputValuesReqProm0!: number;
-  setInputValuesReqProm0(valor: number){
+  setInputValuesReqProm0(valor: number) {
     this.inputValuesReqProm0 = valor;
   }
-  getInputValuesReqProm0(){
+  getInputValuesReqProm0() {
     return this.inputValuesReqProm0;
   }
   inputValuesReqProm1!: number;
-  setInputValuesReqProm1(valor: number){
+  setInputValuesReqProm1(valor: number) {
     this.inputValuesReqProm1 = valor;
   }
-  getInputValuesReqProm1(){
+  getInputValuesReqProm1() {
     return this.inputValuesReqProm1;
   }
   inputValuesReqProm2!: number;
-  setInputValuesReqProm2(valor: number){
+  setInputValuesReqProm2(valor: number) {
     this.inputValuesReqProm2 = valor;
   }
-  getInputValuesReqProm2(){
+  getInputValuesReqProm2() {
     return this.inputValuesReqProm2;
   }
   inputValuesReqProm10!: number;
-  setInputValuesReqProm10(valor: number){
+  setInputValuesReqProm10(valor: number) {
     this.inputValuesReqProm10 = valor;
   }
-  getInputValuesReqProm10(){
+  getInputValuesReqProm10() {
     return this.inputValuesReqProm10;
   }
   inputValuesReqProm11!: number;
-  setInputValuesReqProm11(valor: number){
+  setInputValuesReqProm11(valor: number) {
     this.inputValuesReqProm11 = valor;
   }
-  getInputValuesReqProm11(){
+  getInputValuesReqProm11() {
     return this.inputValuesReqProm11;
   }
   inputValuesReqProm12!: number;
-  setInputValuesReqProm12(valor: number){
+  setInputValuesReqProm12(valor: number) {
     this.inputValuesReqProm12 = valor;
   }
-  getInputValuesReqProm12(){
+  getInputValuesReqProm12() {
     return this.inputValuesReqProm12;
   }
-  setTotalReqProm(valor: number){
+  setTotalReqProm(valor: number) {
     this.totalReqProm = valor;
   }
-  getTotalReqProm(){
+  getTotalReqProm() {
     return this.totalReqProm;
   }
-  setTotalReqProm1(valor: number){
+  setTotalReqProm1(valor: number) {
     this.totalReqProm1 = valor;
   }
-  getTotalReqProm1(){
+  getTotalReqProm1() {
     return this.totalReqProm1;
   }
-  setTotalGastosOp(valor: number){
+  // 8.5
+  inputValuesGastosOp0!: number;
+  setInputValuesGastosOp0(valor: number) {
+    this.inputValuesGastosOp0 = valor;
+  }
+  getInputValuesGastosOp0() {
+    return this.inputValuesGastosOp0;
+  }
+  inputValuesGastosOp1!: number;
+  setInputValuesGastosOp1(valor: number) {
+    this.inputValuesGastosOp1 = valor;
+  }
+  getInputValuesGastosOp1() {
+    return this.inputValuesGastosOp1;
+  }
+  inputValuesGastosOp2!: number;
+  setInputValuesGastosOp2(valor: number) {
+    this.inputValuesGastosOp2 = valor;
+  }
+  getInputValuesGastosOp2() {
+    return this.inputValuesGastosOp2;
+  }
+  setTotalGastosOp(valor: number) {
     this.totalGastosOp = valor;
   }
-  getTotalGastosOp(){
+  getTotalGastosOp() {
     return this.totalGastosOp;
   }
-  setTotalInfr(valor: number){
+  // 8.6
+  inputValuesInfr0!: number;
+  setInputValuesInfr0(valor: number) {
+    this.inputValuesInfr0 = valor;
+  }
+  getInputValuesInfr0() {
+    return this.inputValuesInfr0;
+  }
+  inputValuesInfr1!: number;
+  setInputValuesInfr1(valor: number) {
+    this.inputValuesInfr1 = valor;
+  }
+  getInputValuesInfr1() {
+    return this.inputValuesInfr1;
+  }
+  inputValuesInfr2!: number;
+  setInputValuesInfr2(valor: number) {
+    this.inputValuesInfr2 = valor;
+  }
+  getInputValuesInfr2() {
+    return this.inputValuesInfr2;
+  }
+  //8.6 se invertira
+  inputValuesInfr10!: number;
+  setInputValuesInfr10(valor: number) {
+    this.inputValuesInfr10 = valor;
+  }
+  getInputValuesInfr10() {
+    return this.inputValuesInfr10;
+  }
+  inputValuesInfr11!: number;
+  setInputValuesInfr11(valor: number) {
+    this.inputValuesInfr11 = valor;
+  }
+  getInputValuesInfr11() {
+    return this.inputValuesInfr11;
+  }
+  inputValuesInfr12!: number;
+  setInputValuesInfr12(valor: number) {
+    this.inputValuesInfr12 = valor;
+  }
+  getInputValuesInfr12() {
+    return this.inputValuesInfr12;
+  }
+  setTotalInfr(valor: number) {
     this.totalInfr = valor;
   }
-  getTotalInfr(){
+  getTotalInfr() {
     return this.totalInfr;
   }
-  setTotalInfr1(valor: number){
+  setTotalInfr1(valor: number) {
     this.totalInfr1 = valor;
   }
-  getTotalInfr1(){
+  getTotalInfr1() {
     return this.totalInfr1;
   }
-  setTotalMaq(valor: number){
+
+  // 8.7 inversión propia
+  inputValuesMaq0!: number;
+  setInputValuesMaq0(valor: number) {
+    this.inputValuesMaq0 = valor;
+  }
+  getInputValuesMaq0() {
+    return this.inputValuesMaq0;
+  }
+  inputValuesMaq1!: number;
+  setInputValuesMaq1(valor: number) {
+    this.inputValuesMaq1 = valor;
+  }
+  getInputValuesMaq1() {
+    return this.inputValuesMaq1;
+  }
+  inputValuesMaq2!: number;
+  setInputValuesMaq2(valor: number) {
+    this.inputValuesMaq2 = valor;
+  }
+  getInputValuesMaq2() {
+    return this.inputValuesMaq2;
+  }
+  setTotalMaq(valor: number) {
     this.totalMaq = valor;
   }
-  getTotalMaq(){
+  getTotalMaq() {
     return this.totalMaq;
   }
-  setTotalMaq1(valor: number){
+  //8.7 inversion
+  inputValuesMaq10!: number;
+  setInputValuesMaq10(valor: number) {
+    this.inputValuesMaq10 = valor;
+  }
+  getInputValuesMaq10() {
+    return this.inputValuesMaq10;
+  }
+  inputValuesMaq11!: number;
+  setInputValuesMaq11(valor: number) {
+    this.inputValuesMaq11 = valor;
+  }
+  getInputValuesMaq11() {
+    return this.inputValuesMaq11;
+  }
+  inputValuesMaq12!: number;
+  setInputValuesMaq12(valor: number) {
+    this.inputValuesMaq12 = valor;
+  }
+  getInputValuesMaq12() {
+    return this.inputValuesMaq12;
+  }
+  setTotalMaq1(valor: number) {
     this.totalMaq1 = valor;
   }
-  getTotalMaq1(){
+  getTotalMaq1() {
     return this.totalMaq1;
   }
-  setTotalLegal(valor: number){
+  // 8.8 inversión propia
+  inputValuesLegal0!: number;
+  setInputValuesLegal0(valor: number) {
+    this.inputValuesLegal0 = valor;
+  }
+  getInputValuesLegal0() {
+    return this.inputValuesLegal0;
+  }
+  inputValuesLegal1!: number;
+  setInputValuesLegal1(valor: number) {
+    this.inputValuesLegal1 = valor;
+  }
+  getInputValuesLegal1() {
+    return this.inputValuesLegal1;
+  }
+  inputValuesLegal2!: number;
+  setInputValuesLegal2(valor: number) {
+    this.inputValuesLegal2 = valor;
+  }
+  getInputValuesLegal2() {
+    return this.inputValuesLegal2;
+  }
+  setTotalLegal(valor: number) {
     this.totalLegal = valor;
   }
-  getTotalLegal(){
+  getTotalLegal() {
     return this.totalLegal;
   }
-  setTotalLegal1(valor: number){
+  // 8.8 inversion
+  inputValuesLegal10!: number;
+  setInputValuesLegal10(valor: number) {
+    this.inputValuesLegal10 = valor;
+  }
+  getInputValuesLegal10() {
+    return this.inputValuesLegal10;
+  }
+  inputValuesLegal11!: number;
+  setInputValuesLegal11(valor: number) {
+    this.inputValuesLegal11 = valor;
+  }
+  getInputValuesLegal11() {
+    return this.inputValuesLegal11;
+  }
+  inputValuesLegal12!: number;
+  setInputValuesLegal12(valor: number) {
+    this.inputValuesLegal12 = valor;
+  }
+  getInputValuesLegal12() {
+    return this.inputValuesLegal12;
+  }
+  setTotalLegal1(valor: number) {
     this.totalLegal1 = valor;
   }
-  getTotalLegal1(){
+  getTotalLegal1() {
     return this.totalLegal1;
+  }
+
+  // Total capital operaciones
+  totalCapitalOpApPro!: number;
+  setTotalCapitalOpApPro(valor: number) {
+    this.totalCapitalOpApPro = valor;
+  }
+  getTotalCapitalOpApPro() {
+    return this.totalCapitalOpApPro;
+  }
+  totalCapitalOpInvertira!: number;
+  setTotalCapitalOpInvertira(valor: number) {
+    this.totalCapitalOpInvertira = valor;
+  }
+  getTotalCapitalOpInvertira() {
+    return this.totalCapitalOpInvertira;
+  }
+  // Total capital inversión
+  totalCapitalInversionApPo!: number;
+  setTotalCapitalInversionApPo(valor: number){
+    this.totalCapitalInversionApPo = valor;
+  }
+  getTotalCapitalInversionApPo(){
+    return this.totalCapitalInversionApPo;
+  }
+  totalCapitalInversionSeInvertira!: number;
+  setTotalCapitalInversionSeInvertira(valor: number){
+    this.totalCapitalInversionSeInvertira = valor;
+  }
+  getTotalCapitalInversionSeInvertira(){
+    return this.totalCapitalInversionSeInvertira;
   }
 }
