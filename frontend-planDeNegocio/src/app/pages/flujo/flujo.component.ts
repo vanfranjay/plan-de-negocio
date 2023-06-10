b9633143c61c4ea0bdf3d48acf15dff6763733c4
Richar Irasabal <irasabal.richy@gmail.com>
2023-06-10 02:47:42 -0400

    flujo editado y responsive agregado

--- frontend-planDeNegocio/src/app/pages/flujo/flujo.component.ts
+++ frontend-planDeNegocio/src/app/pages/flujo/flujo.component.ts
@@ -6,5 +6,43 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
   styleUrls: ['./flujo.component.scss']
 })
 export class FlujoComponent {
-
+  colSize: any;
+  colSize2: any;
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+  ){}
+  ngOnInit(){
+    this.breakpointObserver.observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium])
+      .subscribe(result => {
+        if (result.matches) {
+          this.colSize = this.calculateColSize(result.breakpoints);
+          this.colSize2 = this.calculateColSize2(result.breakpoints);
+        } else {
+          this.colSize = 2; // Pantallas más grandes, 4 columnas
+          this.colSize2 = 1;
+        }
+      });
+  }
+  calculateColSize(breakpoints: { [key: string]: boolean }): number {
+    if (breakpoints[Breakpoints.XSmall]) {
+      return 1; // Pantallas extra pequeñas, 1 columna
+    } else if (breakpoints[Breakpoints.Small]) {
+      return 1; // Pantallas pequeñas, 2 columnas
+    } else if (breakpoints[Breakpoints.Medium]) {
+      return 2; // Pantallas medianas, 3 columnas
+    } else {
+      return 2; // Por defecto, 4 columnas
+    }
+  }
+  calculateColSize2(breakpoints: { [key: string]: boolean }): number {
+    if (breakpoints[Breakpoints.XSmall]) {
+      return 1; // Pantallas extra pequeñas, 1 columna
+    } else if (breakpoints[Breakpoints.Small]) {
+      return 1; // Pantallas pequeñas, 2 columnas
+    } else if (breakpoints[Breakpoints.Medium]) {
+      return 1; // Pantallas medianas, 3 columnas
+    } else {
+      return 1; // Por defecto, 4 columnas
+    }
+  }
 }
