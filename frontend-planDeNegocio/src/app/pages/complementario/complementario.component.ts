ecd8e5093e661c3e8f16039c2902c110da6f463d
Richar Irasabal <irasabal.richy@gmail.com>
2023-06-10 01:43:05 -0400

    variables de estado agregados a complementario

--- frontend-planDeNegocio/src/app/pages/complementario/complementario.component.ts
+++ frontend-planDeNegocio/src/app/pages/complementario/complementario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ComplementarioService } from 'src/app/service/complementario/complementario.service';
 
 @Component({
   selector: 'app-complementario',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./complementario.component.scss']
 })
 export class ComplementarioComponent {
+
+  complementoDedicacionEmprendedor!: string;
+  complementoEntiFinancieras!: string;
+  complementoExpAct!: string;
+  complementoGarantia!: string;
+  complementoOtros!: string;
+
+  complementario: string = 'setComplemento';
+  constructor(
+    private complementarioService: ComplementarioService,
+  ) {
+    this.complementoDedicacionEmprendedor = this.complementarioService.getComplementoDedicacionEmprendedor();
+    this.complementoEntiFinancieras = this.complementarioService.getComplementoEntiFinancieras();
+    this.complementoExpAct = this.complementarioService.getComplementoExpAct();
+    this.complementoGarantia = this.complementarioService.getComplementoGarantia();
+    this.complementoOtros = this.complementarioService.getComplementoOtros();
+  }
+  asignarValor(value: string, valor: any) {
+    const valueM: string = this.complementario + value;      //setHome${NombreDeudor}
+    (this.complementarioService as any)[valueM](valor);
+  }
 
 }
--- frontend-planDeNegocio/src/app/service/complementario/complementario.service.ts
+++ frontend-planDeNegocio/src/app/service/complementario/complementario.service.ts
@@ -6,4 +6,40 @@ import { Injectable } from '@angular/core';
 export class ComplementarioService {
 
   constructor() { }
+
+  private complementoDedicacionEmprendedor!: string;
+  public getComplementoDedicacionEmprendedor(): string {
+    return this.complementoDedicacionEmprendedor;
+  }
+  public setComplementoDedicacionEmprendedor(value: string) {
+    this.complementoDedicacionEmprendedor = value;
+  }
+  private complementoEntiFinancieras!: string;
+  public getComplementoEntiFinancieras(): string {
+    return this.complementoEntiFinancieras;
+  }
+  public setComplementoEntiFinancieras(value: string) {
+    this.complementoEntiFinancieras = value;
+  }
+  private complementoExpAct!: string;
+  public getComplementoExpAct(): string {
+    return this.complementoExpAct;
+  }
+  public setComplementoExpAct(value: string) {
+    this.complementoExpAct = value;
+  }
+  private complementoGarantia!: string;
+  public getComplementoGarantia(): string {
+    return this.complementoGarantia;
+  }
+  public setComplementoGarantia(value: string) {
+    this.complementoGarantia = value;
+  }
+  private complementoOtros!: string;
+  public getComplementoOtros(): string {
+    return this.complementoOtros;
+  }
+  public setComplementoOtros(value: string) {
+    this.complementoOtros = value;
+  }
 }
