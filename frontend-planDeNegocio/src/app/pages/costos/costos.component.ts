4d340b47c4bfdd4ad93c8e97352cb65b2c446918
Rodrigo Gutierrez <rodrigo.gutierrez.acc@gmail.com>
2023-06-12 12:19:32 -0400

    Added saving data in Producto o Servicio fields

--- frontend-planDeNegocio/src/app/pages/costos/costos.component.ts
+++ frontend-planDeNegocio/src/app/pages/costos/costos.component.ts
@@ -8,21 +8,69 @@ import { CostosService } from 'src/app/service/costos/costos.service';
   styleUrls: ['./costos.component.scss']
 })
 export class CostosComponent {
-  costos: string = 'setRango'
+  costos: string = 'setCostosDir'
 
   public colSize!: number;
   public colSize2: number = 1;
-  rangoA!: number;
-  rangoB!: number;
-  rangoC!: number;
+  costosDirRangoA!: number;
+  costosDirRangoB!: number;
+  costosDirRangoC!: number;
+
+  costosDirProdServ1!: string;
+  costosDirProdTipo1!: string;
+  costosDirProdCantidad1!: number;
+  costosDirProdUniVenta1!: string;
+  costosDirProdFrecuencia1!: string;
+
+  costosDirProdServ2!: string;
+  costosDirProdTipo2!: string;
+  costosDirProdCantidad2!: number;
+  costosDirProdUniVenta2!: string;
+  costosDirProdFrecuencia2!: string;
+
+  costosDirProdServ3!: string;
+  costosDirProdTipo3!: string;
+  costosDirProdCantidad3!: number;
+  costosDirProdUniVenta3!: string;
+  costosDirProdFrecuencia3!: string;
+
+  costosDirProdServ4!: string;
+  costosDirProdTipo4!: string;
+  costosDirProdCantidad4!: number;
+  costosDirProdUniVenta4!: string;
+  costosDirProdFrecuencia4!: string;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
     private costosService: CostosService,
   ) {
-    this.rangoA = this.costosService.getRangoA();
-    this.rangoB = this.costosService.getRangoB();
-    this.rangoC = this.costosService.getRangoC();
+    this.costosDirRangoA = this.costosService.getCostosDirRangoA();
+    this.costosDirRangoB = this.costosService.getCostosDirRangoB();
+    this.costosDirRangoC = this.costosService.getCostosDirRangoC();
+
+    this.costosDirProdServ1 = this.costosService.getCostosDirProdServ1();
+    this.costosDirProdTipo1 = this.costosService.getCostosDirProdTipo1();
+    this.costosDirProdCantidad1 = this.costosService.getCostosDirProdCantidad1();
+    this.costosDirProdUniVenta1 = this.costosService.getCostosDirProdUniVenta1();
+    this.costosDirProdFrecuencia1 = this.costosService.getCostosDirProdFrecuencia1();
+
+    this.costosDirProdServ2 = this.costosService.getCostosDirProdServ2();
+    this.costosDirProdTipo2 = this.costosService.getCostosDirProdTipo2();
+    this.costosDirProdCantidad2 = this.costosService.getCostosDirProdCantidad2();
+    this.costosDirProdUniVenta2 = this.costosService.getCostosDirProdUniVenta2();
+    this.costosDirProdFrecuencia2 = this.costosService.getCostosDirProdFrecuencia2();
+
+    this.costosDirProdServ3 = this.costosService.getCostosDirProdServ3();
+    this.costosDirProdTipo3 = this.costosService.getCostosDirProdTipo3();
+    this.costosDirProdCantidad3 = this.costosService.getCostosDirProdCantidad3();
+    this.costosDirProdUniVenta3 = this.costosService.getCostosDirProdUniVenta3();
+    this.costosDirProdFrecuencia3 = this.costosService.getCostosDirProdFrecuencia3();
+
+    this.costosDirProdServ4 = this.costosService.getCostosDirProdServ4();
+    this.costosDirProdTipo4 = this.costosService.getCostosDirProdTipo4();
+    this.costosDirProdCantidad4 = this.costosService.getCostosDirProdCantidad4();
+    this.costosDirProdUniVenta4 = this.costosService.getCostosDirProdUniVenta4();
+    this.costosDirProdFrecuencia4 = this.costosService.getCostosDirProdFrecuencia4();
   }
 
   asignarValor(value: string, valor: any) {
@@ -42,7 +90,6 @@ export class CostosComponent {
           this.colSize2 = 1;
         }
       });
-  this.rangoA = this.costosService.getRangoA();
   }
   calculateColSize(breakpoints: { [key: string]: boolean }): number {
     if (breakpoints[Breakpoints.XSmall]) {
@@ -74,6 +121,5 @@ export class CostosComponent {
     this.total = this.inputValues
       .map(value => parseInt(value?.toString() || '0') || 0)
       .reduce((sum, value) => sum + value, 0);
-    this.costosService.setRangoA(this.rangoA)
   }
 }
--- frontend-planDeNegocio/src/app/service/costos/costos.service.ts
+++ frontend-planDeNegocio/src/app/service/costos/costos.service.ts
@@ -7,11 +7,242 @@ export class CostosService {
 
   constructor() { }
 
-  private rangoA!: number;
-  public getRangoA(): number {
-    return this.rangoA;
+  private costosDirRangoA!: number;
+
+  public getCostosDirRangoA(): number {
+    return this.costosDirRangoA;
   }
-  public setRangoA(value: number) {
-    this.rangoA = value;
+
+  public setCostosDirRangoA(value: number) {
+    this.costosDirRangoA = value;
   }
+
+  private costosDirRangoB!: number;
+
+  public getCostosDirRangoB(): number {
+    return this.costosDirRangoB;
+  }
+
+  public setCostosDirRangoB(value: number) {
+    this.costosDirRangoB = value;
+  }
+
+  private costosDirRangoC!: number;
+
+  public getCostosDirRangoC(): number {
+    return this.costosDirRangoC;
+  }
+
+  public setCostosDirRangoC(value: number) {
+    this.costosDirRangoC = value;
+  }
+
+  // Producto Servicio 1
+
+  private costosDirProdServ1!: string;
+
+  public getCostosDirProdServ1(): string {
+    return this.costosDirProdServ1;
+  }
+
+  public setCostosDirProdServ1(value: string) {
+    this.costosDirProdServ1 = value;
+  }
+
+  private costosDirProdTipo1!: string;
+
+  public getCostosDirProdTipo1(): string {
+    return this.costosDirProdTipo1;
+  }
+
+  public setCostosDirProdTipo1(value: string) {
+    this.costosDirProdTipo1 = value;
+  }
+
+  private costosDirProdCantidad1!: number;
+
+  public getCostosDirProdCantidad1(): number {
+    return this.costosDirProdCantidad1;
+  }
+
+  public setCostosDirProdCantidad1(value: number) {
+    this.costosDirProdCantidad1 = value;
+  }
+
+  private costosDirProdUniVenta1!: string;
+
+  public getCostosDirProdUniVenta1(): string {
+    return this.costosDirProdUniVenta1;
+  }
+
+  public setCostosDirProdUniVenta1(value: string) {
+    this.costosDirProdUniVenta1 = value;
+  }
+
+  private costosDirProdFrecuencia1!: string;
+
+  public getCostosDirProdFrecuencia1(): string {
+    return this.costosDirProdFrecuencia1;
+  }
+
+  public setCostosDirProdFrecuencia1(value: string) {
+    this.costosDirProdFrecuencia1 = value;
+  }
+
+   // Producto Servicio 1
+
+   private costosDirProdServ2!: string;
+
+   public getCostosDirProdServ2(): string {
+     return this.costosDirProdServ2;
+   }
+ 
+   public setCostosDirProdServ2(value: string) {
+     this.costosDirProdServ2 = value;
+   }
+ 
+   private costosDirProdTipo2!: string;
+ 
+   public getCostosDirProdTipo2(): string {
+     return this.costosDirProdTipo2;
+   }
+ 
+   public setCostosDirProdTipo2(value: string) {
+     this.costosDirProdTipo2 = value;
+   }
+ 
+   private costosDirProdCantidad2!: number;
+ 
+   public getCostosDirProdCantidad2(): number {
+     return this.costosDirProdCantidad2;
+   }
+ 
+   public setCostosDirProdCantidad2(value: number) {
+     this.costosDirProdCantidad2 = value;
+   }
+ 
+   private costosDirProdUniVenta2!: string;
+ 
+   public getCostosDirProdUniVenta2(): string {
+     return this.costosDirProdUniVenta2;
+   }
+ 
+   public setCostosDirProdUniVenta2(value: string) {
+     this.costosDirProdUniVenta2 = value;
+   }
+ 
+   private costosDirProdFrecuencia2!: string;
+ 
+   public getCostosDirProdFrecuencia2(): string {
+     return this.costosDirProdFrecuencia2;
+   }
+ 
+   public setCostosDirProdFrecuencia2(value: string) {
+     this.costosDirProdFrecuencia2 = value;
+   }
+
+    // Producto Servicio 1
+
+  private costosDirProdServ3!: string;
+
+  public getCostosDirProdServ3(): string {
+    return this.costosDirProdServ3;
+  }
+
+  public setCostosDirProdServ3(value: string) {
+    this.costosDirProdServ3 = value;
+  }
+
+  private costosDirProdTipo3!: string;
+
+  public getCostosDirProdTipo3(): string {
+    return this.costosDirProdTipo1;
+  }
+
+  public setCostosDirProdTipo3(value: string) {
+    this.costosDirProdTipo1 = value;
+  }
+
+  private costosDirProdCantidad3!: number;
+
+  public getCostosDirProdCantidad3(): number {
+    return this.costosDirProdCantidad3;
+  }
+
+  public setCostosDirProdCantidad3(value: number) {
+    this.costosDirProdCantidad3 = value;
+  }
+
+  private costosDirProdUniVenta3!: string;
+
+  public getCostosDirProdUniVenta3(): string {
+    return this.costosDirProdUniVenta3;
+  }
+
+  public setCostosDirProdUniVenta3(value: string) {
+    this.costosDirProdUniVenta1 = value;
+  }
+
+  private costosDirProdFrecuencia3!: string;
+
+  public getCostosDirProdFrecuencia3(): string {
+    return this.costosDirProdFrecuencia3;
+  }
+
+  public setCostosDirProdFrecuencia3(value: string) {
+    this.costosDirProdFrecuencia3 = value;
+  }
+
+   // Producto Servicio 1
+
+   private costosDirProdServ4!: string;
+
+   public getCostosDirProdServ4(): string {
+     return this.costosDirProdServ1;
+   }
+ 
+   public setCostosDirProdServ4(value: string) {
+     this.costosDirProdServ1 = value;
+   }
+ 
+   private costosDirProdTipo4!: string;
+ 
+   public getCostosDirProdTipo4(): string {
+     return this.costosDirProdTipo1;
+   }
+ 
+   public setCostosDirProdTipo4(value: string) {
+     this.costosDirProdTipo1 = value;
+   }
+ 
+   private costosDirProdCantidad4!: number;
+ 
+   public getCostosDirProdCantidad4(): number {
+     return this.costosDirProdCantidad1;
+   }
+ 
+   public setCostosDirProdCantidad4(value: number) {
+     this.costosDirProdCantidad4 = value;
+   }
+ 
+   private costosDirProdUniVenta4!: string;
+ 
+   public getCostosDirProdUniVenta4(): string {
+     return this.costosDirProdUniVenta1;
+   }
+ 
+   public setCostosDirProdUniVenta4(value: string) {
+     this.costosDirProdUniVenta1 = value;
+   }
+ 
+   private costosDirProdFrecuencia4!: string;
+ 
+   public getCostosDirProdFrecuencia4(): string {
+     return this.costosDirProdFrecuencia4;
+   }
+ 
+   public setCostosDirProdFrecuencia4(value: string) {
+     this.costosDirProdFrecuencia4 = value;
+   }
+
 }
