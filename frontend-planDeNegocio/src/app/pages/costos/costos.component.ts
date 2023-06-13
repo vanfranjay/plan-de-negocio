ad57f806f5e4073ca987d4e366382efb1a279360
Richar Irasabal <irasabal.richy@gmail.com>
2023-06-12 20:11:16 -0400

    costos arreglado para pantallas medias o menos

--- frontend-planDeNegocio/src/app/pages/costos/costos.component.ts
+++ frontend-planDeNegocio/src/app/pages/costos/costos.component.ts
@@ -84,36 +84,36 @@ export class CostosComponent {
       .subscribe(result => {
         if (result.matches) {
           this.colSize = this.calculateColSize(result.breakpoints);
-          this.colSize2 = this.calculateColSize2(result.breakpoints);
+          //this.colSize2 = this.calculateColSize2(result.breakpoints);
         } else {
           this.colSize = 3; // Pantallas más grandes, 4 columnas
-          this.colSize2 = 1;
+          //this.colSize2 = 1;
         }
       });
   }
   calculateColSize(breakpoints: { [key: string]: boolean }): number {
     if (breakpoints[Breakpoints.XSmall]) {
-      return 2; // Pantallas extra pequeñas, 1 columna
+      return 3; // Pantallas extra pequeñas, 1 columna
     } else if (breakpoints[Breakpoints.Small]) {
-      return 2; // Pantallas pequeñas, 2 columnas
+      return 3; // Pantallas pequeñas, 2 columnas
     } else if (breakpoints[Breakpoints.Medium]) {
       return 3; // Pantallas medianas, 3 columnas
     } else {
       return 3; // Por defecto, 4 columnas
     }
   }
-  calculateColSize2(breakpoints: { [key: string]: boolean }): number {
-    if (breakpoints[Breakpoints.XSmall]) {
-      return 2; // Pantallas extra pequeñas, 1 columna
-    } else if (breakpoints[Breakpoints.Small]) {
-      return 2; // Pantallas pequeñas, 2 columnas
-    } else if (breakpoints[Breakpoints.Medium]) {
-      return 1; // Pantallas medianas, 3 columnas
-    } else {
-      return 1; // Por defecto, 4 columnas
-    }
-  }
-  rows: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+//  calculateColSize2(breakpoints: { [key: string]: boolean }): number {
+//    if (breakpoints[Breakpoints.XSmall]) {
+//      return 2; // Pantallas extra pequeñas, 1 columna
+//    } else if (breakpoints[Breakpoints.Small]) {
+//      return 2; // Pantallas pequeñas, 2 columnas
+//    } else if (breakpoints[Breakpoints.Medium]) {
+//      return 1; // Pantallas medianas, 3 columnas
+//    } else {
+//      return 1; // Por defecto, 4 columnas
+//    }
+//  }
+//  rows: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   inputValues: (number | null)[] = [];
   total: number = 0;
 
